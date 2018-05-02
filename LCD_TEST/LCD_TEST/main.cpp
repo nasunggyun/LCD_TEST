29a4bf70c6e92171e9274e1a6ea2a3e3c3a53b51
nasunggyun <nasunggyun@gmail.com>
2018-05-02 16:02:33 +0900

    LED 좌우로 7번씩 쉬프트

--- LCD_TEST/LCD_TEST/main.cpp
+++ LCD_TEST/LCD_TEST/main.cpp
@@ -15,10 +15,16 @@ int main(void)
    	PORTD = 0xff;
     while (1) 
     {
-				PORTD = 0x00;
-				_delay_ms(300);
-				PORTD = 0xff;
-				_delay_ms(300);
-    }
+		for (int i=0; i<7; i++)
+		{
+			PORTD = ~(1<<i);
+			_delay_ms(200);
+		}
+		for (int i=0; i<7; i++)
+		{
+			PORTD = ~(0x80>>i);
+			_delay_ms(200);
+		}
+	 }
 }
 
